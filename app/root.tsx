54653eee61403c1d52055987052bdfe1cbb35a37
synuns <identityjang@gmail.com>
2025-08-13 16:40:47 +0900

    fix: remove ScrollRestoration component from Layout
    - Layout 컴포넌트에서 ScrollRestoration 컴포넌트를 제거하여 불필요한 요소를 정리하고 코드 간결성 향상

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,6 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
 } from "react-router";
 
 import type { Route } from "./+types/root";
@@ -34,7 +33,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        <ScrollRestoration />
         <Scripts />
       </body>
     </html>
